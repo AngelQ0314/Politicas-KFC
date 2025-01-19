6ee9353005b7571a2d1e8d02bcddac90df8e4ff5
AngelQ0314 <leonelquishpe0314@gmail.com>
2025-01-19 07:47:55 -0500

    back

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: '127.0.0.1',
+      port: 1433,
+      username: 'angel_0314', 
+      password: '0314',
+      database: 'MAXPOINT_K015_17072024_4.0.9.4_DOMICILIO',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, 
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+      },
+    }),
+  ],
+})
+export class AppModule {}
